5914070002c0d5883db5b3e1a41369e6c4c9fa59
csjits <christoph.sachsenhausen@gmail.com>
2015-06-06 01:33:37 +0200

    Try to generate user hash from android id / build serial

--- app/src/main/java/app/indiana/helpers/UserHelper.java
+++ app/src/main/java/app/indiana/helpers/UserHelper.java
@@ -1,5 +1,9 @@
 package app.indiana.helpers;
 
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -10,13 +14,19 @@ import java.util.Date;
  */
 public class UserHelper {
 
-    public static String createUserHash() {
-        Date now = new Date();
-        SecureRandom sr = new SecureRandom();
-        byte[] output = new byte[16];
-        sr.nextBytes(output);
-        String hash = md5(now.toString() + output.toString());
-        return hash;
+    public static String createUserHash(Context context) {
+        String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        if (androidId != null && !androidId.equals("") && !androidId.equalsIgnoreCase("9774d56d682e549c")) {
+            return md5(androidId);
+        } else if (Build.SERIAL != null && !Build.SERIAL.equals("")) {
+            return md5(Build.SERIAL);
+        } else {
+            SecureRandom sr = new SecureRandom();
+            byte[] output = new byte[16];
+            sr.nextBytes(output);
+            return md5(output.toString());
+        }
+
     }
 
     private static String md5(String s) {
