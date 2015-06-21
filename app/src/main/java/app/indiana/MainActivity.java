6faf2b85f3a6ba6e06d78a924d187f72d52ec28d
csjits <christoph.sachsenhausen@gmail.com>
2015-06-22 00:39:40 +0200

    Cleanup

--- app/src/main/java/app/indiana/MainActivity.java
+++ app/src/main/java/app/indiana/MainActivity.java
@@ -12,8 +12,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
