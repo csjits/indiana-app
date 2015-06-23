ca5662c4b76c84091f1c0f01e7c5c7fb2fb595b8
csjits <christoph.sachsenhausen@gmail.com>
2015-06-24 00:12:37 +0200

    Make serializable

--- app/src/main/java/app/indiana/models/PostContainer.java
+++ app/src/main/java/app/indiana/models/PostContainer.java
@@ -1,9 +1,11 @@
 package app.indiana.models;
 
+import java.io.Serializable;
+
 /**
  * Created by chris on 07.05.2015.
  */
-public class PostContainer {
+public class PostContainer implements Serializable {
     public String id;
     public String message;
     public String age;
