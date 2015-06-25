f42cdd5bb70e37f5b8012f50b0cccc82ae3f3b44
csjits <christoph.sachsenhausen@gmail.com>
2015-06-26 01:00:20 +0200

    Notification final styling and strings

--- app/src/main/java/app/indiana/services/NotificationService.java
+++ app/src/main/java/app/indiana/services/NotificationService.java
@@ -1,10 +1,15 @@
 package app.indiana.services;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.location.Location;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
@@ -43,28 +48,44 @@ public class NotificationService extends BroadcastReceiver {
                         showMyPostActivityNotification(context, posts.get(i), diffs.get(i));
                     }
                 }
+                postCacheService.cache("my", response);
             }
         });
     }
 
     private void showMyPostActivityNotification(Context context, PostContainer post, int change) {
-        NotificationCompat.Builder notifictationBuilder = new NotificationCompat.Builder(context);
 
-        String title = "New ";
+        Resources res = context.getResources();
+        String title = res.getString(R.string.notification_my_title_new) + " ";
         if (change % (PostCacheService.SCORE_DIFF_MULTI * PostCacheService.REPLIES_DIFF_MULTI) == 0) {
-            title += "replies and ratings";
+            title += res.getString(R.string.notification_my_title_ratings_replies);
         } else if (change % PostCacheService.SCORE_DIFF_MULTI == 0) {
-            title += "ratings";
+            title += res.getString(R.string.notification_my_title_ratings);
         } else if (change % PostCacheService.REPLIES_DIFF_MULTI == 0) {
-            title += "replies";
+            title += res.getString(R.string.notification_my_title_replies);
         }
 
-        String message = (post.message.length() > 50) ? post.message.substring(0, 50)+"…" : post.message;
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.mipmap.notification_icon_large);
 
-        notifictationBuilder.setContentTitle(title);
-        notifictationBuilder.setContentText("\"" + message + "\"\r\nScore: " + post.score + "\r\nReplies: " + post.replies);
-        notifictationBuilder.setSmallIcon(R.drawable.ic_indiana);
-        notifictationBuilder.setContentIntent(buildIntent(context, MainActivity.class));
+        NotificationCompat.BigTextStyle style = new NotificationCompat.BigTextStyle();
+        style.setBigContentTitle(title);
+        style.bigText(post.message);
+        style.setSummaryText(
+                res.getString(R.string.notification_my_summary_score, post.score)
+                        + " \t "
+                        + res.getString(R.string.notification_my_summary_replies, post.replies));
+
+        NotificationCompat.Builder notifictationBuilder = new NotificationCompat.Builder(context)
+            .setStyle(style)
+            .setContentTitle(title)
+            .setContentText(post.message)
+            .setSmallIcon(R.mipmap.notification_icon_small)
+            .setLargeIcon(largeIcon)
+            .setAutoCancel(true)
+            .setVibrate(new long[]{0, 100, 100, 100})
+            .setColor(res.getColor(R.color.color_primary))
+            .setLights(Color.CYAN, 400, 2000)
+            .setContentIntent(buildIntent(context, MainActivity.class));
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(post.id, 1, notifictationBuilder.build());
